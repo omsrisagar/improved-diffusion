58714ebe77f5095da5b7d8160651fdb7ea1b8006
Srikanth Vidyasagar Sadhu <srikanthvidyasagar.sadhu@sri.com>
2022-08-11 14:33:54 -0700

    Removed internal directory populating from blobfile for data directories

--- improved_diffusion/image_datasets.py
+++ improved_diffusion/image_datasets.py
@@ -1,3 +1,5 @@
+import os.path
+
 from PIL import Image
 import blobfile as bf
 from mpi4py import MPI
@@ -61,8 +63,8 @@ def _list_image_files_recursively(data_dir):
         ext = entry.split(".")[-1]
         if "." in entry and ext.lower() in ["jpg", "jpeg", "png", "gif"]:
             results.append(full_path)
-        elif bf.isdir(full_path):
-            results.extend(_list_image_files_recursively(full_path))
+        # elif bf.isdir(full_path):
+        #     results.extend(_list_image_files_recursively(full_path))
     return results
 
 
